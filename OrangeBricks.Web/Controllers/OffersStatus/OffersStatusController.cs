52fcdb619a93e7d57eeba4e56242ac8de64791c8
adrian fearon <adrian_fearon@yahoo.co.uk>
2017-04-11 20:25:56 +0100

    A) added ViewModel and link for buyer offer status.

---
+++ OrangeBricks.Web/Controllers/OffersStatus/OffersStatusController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OrangeBricks.Web.Models;
+
+namespace OrangeBricks.Web.Controllers.Offers
+{
+    public class OffersStatusController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: OffersStatus
+        public ActionResult Index()
+        {
+            return View(db.Offers.ToList());
+        }
+
+        // GET: OffersStatus/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Offer offer = db.Offers.Find(id);
+            if (offer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(offer);
+        }
+
+        // GET: OffersStatus/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OffersStatus/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Amount,Status,CreatedAt,UpdatedAt")] Offer offer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Offers.Add(offer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(offer);
+        }
+
+        // GET: OffersStatus/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Offer offer = db.Offers.Find(id);
+            if (offer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(offer);
+        }
+
+        // POST: OffersStatus/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Amount,Status,CreatedAt,UpdatedAt")] Offer offer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(offer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(offer);
+        }
+
+        // GET: OffersStatus/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Offer offer = db.Offers.Find(id);
+            if (offer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(offer);
+        }
+
+        // POST: OffersStatus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Offer offer = db.Offers.Find(id);
+            db.Offers.Remove(offer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
